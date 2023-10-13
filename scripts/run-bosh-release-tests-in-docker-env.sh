a669b768b7f8d22d5dc01dd2be06f4985514a30b
Diego Lemos <diego@diegolemos.net>
2023-10-13 14:25:55 +0100

    [CI| Change some images following Runway migration (#167)
    * [ci] Pull main-bosh-docker image from Harbor
    
    * [ci] switch image type to registry-image
    
    * [ci] switch docker tests to use essentials image

--- scripts/run-bosh-release-tests-in-docker-env.sh
+++ scripts/run-bosh-release-tests-in-docker-env.sh
@@ -31,5 +31,5 @@ run \
 --env BOSH_CLIENT_SECRET=${BOSH_CLIENT_SECRET} \
 --env BOSH_CA_CERT=${BOSH_CA_CERT} \
 --env MAPFS_RELEASE_PATH=/mapfs-release \
-"${DOCKER_IMAGE:-cfpersi/bosh-release-tests}" \
+harbor-repo.vmware.com/cryogenics/essentials \
 $COMMAND_TO_RUN
